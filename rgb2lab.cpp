10f4ef05cbb377d0d367a6a152c26759bc34f5f2
Lettergram <Lettergramm@gmail.com>
2014-10-31 15:10:55 -0500

    Correctly identfies alpha and beta (inverse)

--- rgb2lab.cpp
+++ rgb2lab.cpp
@@ -4,7 +4,6 @@
  * To Execute g++ -I/usr/include -L/usr/lib -lopencv_core.2.4.9 -lopencv_calib3d.2.4.9 -lopencv_imgproc.2.4.9 -lopencv_highgui.2.4.9 rgb2lab.cpp -o rgb2lab
  */
 
-#include <stdio.h>
 #include <iostream>
 
 #include <opencv/cv.h>
@@ -17,9 +16,9 @@ using namespace cv;
  * Takes in a RGB image and outputs
  * a Lab space image.
  **/
-Mat BGR2LAB(Mat const &imgRGB){
+Mat RGB2LAB(Mat const &imgRGB){
   Mat imgLab;
-  cvtColor(imgRGB, imgLab, CV_RGB2Lab);
+  cvtColor(imgRGB, imgLab, CV_BGR2Lab);
   return imgLab;
 }
 
@@ -30,6 +29,7 @@ Mat BGR2LAB(Mat const &imgRGB){
  **/
 Mat alphaLAB(Mat const &imgLab){
 
+  //Mat alphaLab(imgLab.size().height, imgLab.size().width, CV_8UC3); // may not work
   Mat alphaLab;
   cvtColor(imgLab, alphaLab, CV_Lab2RGB);
   Size s = alphaLab.size();
@@ -39,9 +39,9 @@ Mat alphaLAB(Mat const &imgLab){
 
   for(int y = 0; y < s.height; y++){
     for(int x = 0; x < s.width; x++){
-      (*alphaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels * x + 1];
-      (*alphaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels * x + 1];
-      (*alphaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels * x + 1];
+      (*alphaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels*x + 1];
+      (*alphaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels*x + 1];
+      (*alphaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels*x + 1];
     }
   }
   return alphaLab;
@@ -60,17 +60,17 @@ Mat betaLAB(Mat &imgLab){
   int channels = imgLab.channels();
   for(int y = 0; y < s.height; y++){
     for(int x = 0; x < s.width; x++){
-      (*betaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels * x + 2];
-      (*betaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels * x + 2];
-      (*betaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels * x + 2];
+      (*betaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels*x + 2];
+      (*betaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels*x + 2];
+      (*betaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels*x + 2];
     }
   }
   return betaLab;
 }
 
 int main(int argc, const char* argv[]){
-
-  Mat imgLab =  BGR2LAB(imread(argv[1], CV_LOAD_IMAGE_COLOR)); 
+  
+   Mat imgLab =  RGB2LAB(imread(argv[1], CV_LOAD_IMAGE_COLOR));
 
   char filename[60];
 
@@ -80,6 +80,5 @@ int main(int argc, const char* argv[]){
   imwrite(filename, alphaLAB(imgLab));
   sprintf(filename, "output/%s-betaLab.jpg", argv[1]);
   imwrite(filename, betaLAB(imgLab));
-
   return 0;
 }
